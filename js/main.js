03033dc88ebc64a6d408f57f3d84a4ca87aa68f1
Michael Cacciano <michael.cacciano@gmail.com>
2019-05-11 10:36:57 -0400

    added sass and did some refactoring mostly to header and navbar

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+var navIcon = document.querySelector('#hamburger-icon');
+var navbar = document.querySelector('.navbar');
+
+navIcon.addEventListener('click', function() {
+  this.classList.toggle('open');
+
+  // navbar.classList.toggle('navbar');
+  // navbar.classList.toggle('mobile-nav');
+
+  navbar.style.display = navbar.style.display === '' ? 'block' : '';
+});
 
 var map;
 function initMap() {
-  var uluru = {lat: 39.090517, lng: -108.562416};
+  var uluru = { lat: 39.090517, lng: -108.562416 };
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 39.090517, lng: -108.562416},
+    center: { lat: 39.090517, lng: -108.562416 },
     zoom: 8
   });
-  var marker = new google.maps.Marker({position : uluru, map: map})
+  var marker = new google.maps.Marker({ position: uluru, map: map });
 }
